4620f651e2619a4c7fa335c09ac96044d536f626
shehzad-haroon <56392887+shehzad-haroon@users.noreply.github.com>
2021-05-09 15:57:29 +0500

    changes in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,28 +2,10 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi shehzad</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
+const index = () => (
+  <div>
+    <h1>Error 404 </h1>
+      </div>
 )
 
-export default IndexPage
+export default index
